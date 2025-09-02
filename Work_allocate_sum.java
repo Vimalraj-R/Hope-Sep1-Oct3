b05c3fbcd72e4fe8a019eeba3d17819d8e822987
Vimalraj Ravi <161675933+Vimalraj-R@users.noreply.github.com>
2025-09-02 09:45:55 +0530

    Update Work_allocate_sum.java

--- Work_allocate_sum.java
+++ Work_allocate_sum.java
@@ -35,3 +35,64 @@ public class Solution {
         System.out.print(total);
     }
 }
+
+
+
+
+// Another approach
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N = sc.nextInt();
+        char[] jobs=new char[N];
+        int[] dl = new int[N];
+        int[] p = new int[N];
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<N; i++)
+        {
+            char[i]=sc.next()
+            dl[i] = sc.nextInt();
+            p[i] = sc.nextInt();
+            if(dl[i]>max){
+                max=dl[i];
+            }
+        }
+        for(int i=0; i<N; i++)
+        {
+            for(int j=i+1; j<N; j++)
+            {
+                if(p[i]<p[j]){
+                    int t = p[i];
+                    p[i] = p[j];
+                    p[j] = t;
+                    
+                    t = dl[i];
+                    dl[i] = dl[j];
+                    dl[j] = t;
+                }
+            }
+        }
+        int[] ans = new int[max+1];
+        for(int i=0; i<N; i++)
+        {
+            for(int j=dl[i]; j>0; j--)
+            {
+               if(ans[j]==0){
+                   ans[j]=p[i];
+                   break;
+               }
+            }
+        }
+        int ns=0;
+        for(int i=0; i<max+1; i++)
+        {
+            ns+=ans[i];
+        }
+        System.out.println(ns);
+    }
+}
